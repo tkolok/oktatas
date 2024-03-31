acdd2446dea690a878b0479adb9de15bcc72ecb9
Hadas Tamás <hadastk@gmail.com>
2024-03-31 14:18:15 +0200

    Fix typos

--- javascript/02/homework.js
+++ javascript/02/homework.js
@@ -7,7 +7,7 @@ if (a === b) {
     console.log('Nem egyeznek meg');
 }
 
-// Írj kód részletet, amelyben kiiratod a 100-nál kisebb 5-tel osztható pozitív számokat!
+// Írj kód részletet, amelyben kiíratod a 100-nál kisebb 5-tel osztható pozitív számokat!
 // a)
 for (let number = 1; number < 100; number++) {
     if (number % 5 === 0) {
@@ -28,19 +28,19 @@ for (let number = 5; number < 100; number += 5) {
     console.log(number);
 }
 
-// Írj kód részletet, amelyben kiiratod a 100-nál kisebb pozitív számok összegét!
+// Írj kód részletet, amelyben kiíratod a 100-nál kisebb pozitív számok összegét!
 let sum = 0;
 for (let number = 1; number < 100; number++) {
     sum += number;
 }
 console.log(sum);
 
-// Írj kód részletet, amelyben kiiratod az első 10 pozitív szám négyzetét!
+// Írj kód részletet, amelyben kiíratod az első 10 pozitív szám négyzetét!
 for (let number = 1; number <= 10; number++) {
     console.log(number * number);
 }
 
-// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd kiiratod a legnagyobb számot közülük!
+// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd kiíratod a legnagyobb számot közülük!
 let array = [30, 70, 21, 65, 8];
 let max = array[0];
 for (let i = 1; i < array.length; i++) {
@@ -50,7 +50,7 @@ for (let i = 1; i < array.length; i++) {
 }
 console.log(max);
 
-// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd kiiratod, hogy hányszor szerepelt a 4-es a tömbben!
+// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd kiíratod, hogy hányszor szerepelt a 4-es a tömbben!
 let array = [5, 4, 3, 4, 5];
 let count = 0;
 for (let i = 0; i < array.length; i++) {
@@ -60,7 +60,7 @@ for (let i = 0; i < array.length; i++) {
 }
 console.log(count);
 
-// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd az összes elemére kiiratod, hogy osztható 2-vel, 3-mal és 5-tel!
+// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd az összes elemére kiíratod, hogy osztható 2-vel, 3-mal és 5-tel!
 let array = [30, 70, 21, 65, 8];
 for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 === 0) {
@@ -74,7 +74,7 @@ for (let i = 0; i < array.length; i++) {
     }
 }
 
-// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd visszafelé kiiratod az elemeit!
+// Írj kód részletet, amelyben létrehozol egy 5 elemű szám tömböt, majd visszafelé kiíratod az elemeit!
 // a)
 let array = [1, 2, 3, 4, 5];
 for (let i = array.length - 1; i >= 0; i--) {
@@ -100,7 +100,7 @@ if (array.length % 2 === 0) {
     }
 }
 
-// Írj kód részletet, amelyben kiiratod a 100-nál kisebb Fibonacci számokat!
+// Írj kód részletet, amelyben kiíratod a 100-nál kisebb Fibonacci számokat!
 let a = 0;
 let b = 1;
 while (b < 100) {
@@ -109,13 +109,13 @@ while (b < 100) {
     a = b - a;
 }
 
-// Írj kód részletet, amelyben kiiratod egy 10 elemű tömb minden második elemét!
+// Írj kód részletet, amelyben kiíratod egy 10 elemű tömb minden második elemét!
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i = 0; i < array.length; i += 2) {
     console.log(array[i]);
 }
 
-// Írj kód részletet, amelyben kiiratod létrehozol 2 darab 5 elemű szám tömböt, majd létrehozol egy üres tömböt! Az üres tömbbe a másik 2 tömb azonos indexű
+// Írj kód részletet, amelyben kiíratod létrehozol 2 darab 5 elemű szám tömböt, majd létrehozol egy üres tömböt! Az üres tömbbe a másik 2 tömb azonos indexű
 // elemeit összeadod!
 let a1 = [1, 2, 3, 4, 5];
 let a2 = [5, 4, 3, 2, 1];
